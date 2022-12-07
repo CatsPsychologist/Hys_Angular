4921c6d2208e273545a8607fa4ca1f2291d90b1e
CatsPsychologist <mainofficee@gmail.com>
2022-12-07 15:58:31 +0200

    ls_15 products module route

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from "./products/products.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products',
+  },
+  {
+    path: 'products',
+    pathMatch: 'full',
+    loadChildren: () => ProductsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
