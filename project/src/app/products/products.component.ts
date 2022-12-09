f907b95a28589163a13ab006d0e98498416bc7fa
CatsPsychologist <mainofficee@gmail.com>
2022-12-09 21:13:39 +0200

    ls_15 products migration

--- project/src/app/products/products.component.ts
+++ project/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {products} from "../mock/products";
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+  public products = products;
 }
