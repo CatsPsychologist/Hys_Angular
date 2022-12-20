402a4b24cbb2452778965802cf89a49272f68b87
CatsPsychologist <mainofficee@gmail.com>
2022-12-20 15:43:38 +0200

    ls_16 fixed merge consiquences, deleted useless folder

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -12,16 +12,13 @@ import {SharedModule} from "./pages/shop/shared/shared.module";
   declarations: [
     AppComponent,
   ],
-<<<<<<< HEAD:app/src/app/app.module.ts
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
   ],
-=======
-    imports: [
-    ],
->>>>>>> e92d5f18eccbf220ec28fbfee08c0ba52397994d:project/src/app/app.module.ts
+
   providers: [],
   bootstrap: [AppComponent]
 })
